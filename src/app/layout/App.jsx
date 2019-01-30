9c6b0ccfec31f1d446b9f68f2d3098a1dba05527
skybsky <zoomzoom171@gmail.com>
2019-01-30 11:27:26 +0300

    initial commit

---
+++ src/app/layout/App.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+       <h1>Test</h1>
+      </div>
+    );
+  }
+}
+
+export default App;
